6a5619ff4a32e869050018ce5c84fb32812c7546
shahidctp <shahidpyn@gmail.com>
2022-04-11 12:17:59 +0530

    updation

--- api/views.py
+++ api/views.py
@@ -12,8 +12,14 @@ def get_api(request):
         data = request.data
         address = data.get('address')
         output = data.get('output_format')
-        formatting = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key=AIzaSyCOD3KvY2DDzEfel-NZ_LKIWXr86EF_EUw'
-        r = requests.get(formatting)
-        return JsonResponse(r, safe=False)
+        formatting = f'https://maps.googleapis.com/maps/api/geocode/{output}?address={address}&key=AIzaSy' \
+                     f'COD3KvY2DDzEfel-NZ_LKIWXr86EF_EUw'
+        print(formatting)
+        response = requests.get(url=formatting)
 
-
+        response = response.json()
+        coordinates = response['results'][0]['geometry']['location']
+        data = {}
+        data.update({"coordinates": coordinates})
+        data.update({"address": address})
+        return JsonResponse(data, safe=False)
